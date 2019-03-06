c83fa8636117c49ba68f6dba6c3e051ae5aec6e7
Telejuan <juan.sanchez.13@sansano.usm.cl>
2019-03-06 16:37:02 -0300

    updating manifest

--- mqtt_abstract_interface/__manifest__.py
+++ mqtt_abstract_interface/__manifest__.py
@@ -1,13 +1,22 @@
 {
     'name' : 'Mqtt Abstract Interface',
+	'summary' : 'Technical Module for MQTT',
     'description' : 'Technical module for developers that provides Mqtt functionalities to their models through the inheritance of an abstract model.',
     'author' : 'Tectronix Spa',
     'depends' : ['base'],
     'version' : '0.3.0',
     'license' : 'LGPL-3',
     'data' : [],
+	'category' : 'Extra Tools',
+	'website' : 'https://www.tectronix.cl',
     'support' : 'jsanchez@tectronix.cl',
     "external_dependencies": {
         'python': ['paho']
     },
+	'contributors' : [
+		'Juan Sanchez <Jsanchez@tectronix.cl>',
+	],
+	'images' : [
+		'static/description/banner.png',
+	],
 }
